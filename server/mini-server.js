636240757e8d9809723242e1c5b680dc681e56a7
elina <elinapark0818@gmail.com>
2022-01-08 19:04:52 +0900

    Add bady-parser

--- server/mini-server.js
+++ server/mini-server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+// * v4.16.0 기준으로 이미 빌트인 되어 있지만.. 설치해봤따
+// const bodyParser = require("body-parser");
 const app = express();
+
+app.use("/static", express.static(__dirname + "../client"));
 
 // * 모든 요청에 대한 CORS 처리
 // app.use(express.static("client"));
@@ -8,6 +12,16 @@ app.use(cors());
 
 // * 배열과 객체만 허용되는 것을 비활성화
 app.use(express.json({ strict: false }));
+
+// * body-parser
+// ? json 형태로 파싱하기
+// app.use(bodyParser().json());
+// // ? false : 기본으로 내장된 querystring 모듈을 사용
+// app.use(express.urlencoded({ extended: false }));
+// app.post("/", (req, res) => {
+//   const post = req.body;
+//   console.log(post);
+// });
 
 // * 포트설정
 const ip = "localhost";
@@ -38,4 +52,9 @@ app.post("/lower", (req, res) => {
 // todo: 클라이언트 에러(400)는 err 인자를 안받는다.
 app.use((req, res, next) => {
   res.status(404).send("404 Error! Not Found");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
